4cb6e70fe76db69bd66824ad7909e435b9df95ad
Seheon Yu <seheon@seoultech.ac.kr>
2024-05-13 14:53:53 +0900

    chore: Update home page layout and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { Text, Strong } from "@/components/catalyst";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-10">
+      <Strong className="text-6xl">Vocabulary Workshop</Strong>
+      <Text>
+        <Strong>Click</Strong> anywhere to start or <Strong>Scroll</Strong> to
+        see the vocabularies
+      </Text>
+    </main>
   );
 }
