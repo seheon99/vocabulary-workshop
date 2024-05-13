5726802a6aa04a12a9b89db332bc5e24bf60e9af
Seheon Yu <seheon@seoultech.ac.kr>
2024-05-13 17:02:34 +0900

    feature: Add vocabularies in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,73 @@
-import { Text, Strong } from "@/components";
+import {
+  Text,
+  Strong,
+  Link,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableHeader,
+} from "@/components";
+import { categoryService } from "@/features/category/category.service";
+import { termService } from "@/features/term/term.service";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await categoryService.findAll();
+  const terms = await termService.findAll();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-10">
-      <Strong className="text-6xl">Vocabulary Workshop</Strong>
-      <Text>
-        <Strong>Click</Strong> anywhere to start or <Strong>Scroll</Strong> to
-        see the vocabularies
-      </Text>
+    <main className="max-h-screen snap-y overflow-y-scroll">
+      <Link href="/term">
+        <div className="flex min-h-screen shrink-0 snap-center flex-col items-center justify-center gap-10">
+          <Strong className="text-6xl">Vocabulary Workshop</Strong>
+          <Text>
+            <Strong>Click</Strong> anywhere to start or <Strong>Scroll</Strong>{" "}
+            to see the vocabularies
+          </Text>
+        </div>
+      </Link>
+
+      <div className="max-h-screen shrink-0 snap-center overflow-y-scroll p-20">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableHeader>Category</TableHeader>
+              <TableHeader>Term</TableHeader>
+              <TableHeader>Definition</TableHeader>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {terms.map(async (t) => (
+              <TableRow key={t.id}>
+                <TableCell>
+                  {categories.find(({ id }) => id === t.categoryId)?.name}
+                </TableCell>
+                <TableCell>{t.name}</TableCell>
+                <TableCell>{t.definition}</TableCell>
+              </TableRow>
+            ))}
+            {terms.map(async (t) => (
+              <TableRow key={t.id}>
+                <TableCell>
+                  {categories.find(({ id }) => id === t.categoryId)?.name}
+                </TableCell>
+                <TableCell>{t.name}</TableCell>
+                <TableCell>{t.definition}</TableCell>
+              </TableRow>
+            ))}
+            {terms.map(async (t) => (
+              <TableRow key={t.id}>
+                <TableCell>
+                  {categories.find(({ id }) => id === t.categoryId)?.name}
+                </TableCell>
+                <TableCell>{t.name}</TableCell>
+                <TableCell>{t.definition}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </main>
   );
 }
