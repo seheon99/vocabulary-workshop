ed6be1377b50cbba55cdda7b996ddcf447d49712
Seheon Yu <seheon@seoultech.ac.kr>
2024-05-15 05:58:15 +0900

    refactor: Remove color property in Strong text

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -2,28 +2,6 @@ import { clsx } from "clsx";
 
 import { Link } from "./link";
 
-const colors = {
-  zinc: "text-zinc-950 dark:text-white",
-
-  red: "text-red-700 dark:text-red-400",
-  orange: "text-orange-700 dark:text-orange-400",
-  amber: "text-amber-700 dark:text-amber-400",
-  yellow: "text-yellow-700 dark:text-yellow-300",
-  lime: "text-lime-700 dark:text-lime-300",
-  green: "text-green-700 dark:text-green-400",
-  emerald: "text-emerald-700 dark:text-emerald-400",
-  teal: "text-teal-700 dark:text-teal-300",
-  cyan: "text-cyan-700 dark:text-cyan-300",
-  sky: "text-sky-700 dark:text-sky-300",
-  blue: "text-blue-700 dark:text-blue-400",
-  indigo: "text-indigo-700 dark:text-indigo-400",
-  violet: "text-violet-700 dark:text-violet-400",
-  purple: "text-purple-700 dark:text-purple-400",
-  fuchsia: "text-fuchsia-700 dark:text-fuchsia-400",
-  pink: "text-pink-700 dark:text-pink-400",
-  rose: "text-rose-700 dark:text-rose-400",
-};
-
 export function Text({
   className,
   ...props
@@ -56,14 +34,13 @@ export function TextLink({
 }
 
 export function Strong({
-  color = "zinc",
   className,
   ...props
-}: React.ComponentPropsWithoutRef<"strong"> & { color?: keyof typeof colors }) {
+}: React.ComponentPropsWithoutRef<"strong">) {
   return (
     <strong
       {...props}
-      className={clsx(className, colors[color], "font-medium")}
+      className={clsx(className, "font-medium text-zinc-950 dark:text-white")}
     />
   );
 }
