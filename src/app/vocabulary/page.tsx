da8e2ef8be38b41543c2b386800756acc958da50
Seheon Yu <seheon@seoultech.ac.kr>
2024-05-16 15:31:49 +0900

    fix: Update vocabulary page component to get random term

--- src/app/vocabulary/page.tsx
+++ src/app/vocabulary/page.tsx
@@ -1,13 +1,48 @@
-import { notFound, redirect } from "next/navigation";
+"use client";
 
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import {
+  Alert,
+  AlertActions,
+  AlertDescription,
+  AlertTitle,
+  Button,
+} from "@/components";
 import { getRandomTerm } from "@/features/term/term.repository";
 
-export default async function Terms() {
-  const terms = await getRandomTerm();
+export default function Terms() {
+  const [error, setError] = useState<string | null>(null);
 
-  if (!terms) {
-    notFound();
-  }
+  const router = useRouter();
 
-  redirect(`/vocabulary/${terms.id}`);
+  useEffect(() => {
+    async function redirectRandom() {
+      const term = await getRandomTerm();
+      if (term) {
+        router.replace(`/vocabulary/${term.id}`);
+      } else {
+        setError("No terms found, please add term before use quiz.");
+      }
+    }
+    if (!error) {
+      redirectRandom();
+    }
+  }, [router, error]);
+
+  return (
+    <Alert open={!!error} onClose={() => setError(null)}>
+      <AlertTitle>Something went wrong :(</AlertTitle>
+      <AlertDescription>{error}</AlertDescription>
+      <AlertActions>
+        <Button plain onClick={() => router.push("/")}>
+          Go home
+        </Button>
+        <Button onClick={() => router.push("/vocabulary/add")}>
+          Add new term
+        </Button>
+      </AlertActions>
+    </Alert>
+  );
 }
