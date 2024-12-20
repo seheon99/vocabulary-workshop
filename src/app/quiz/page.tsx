42a07f74555db55a045208e803dbf02544745c56
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2024-12-20 13:45:52 +0000

    Add user verify login before quiz

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -2,26 +2,34 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { toast } from "react-toastify";
 import useSWR from "swr";
 
 import { findRandomVocabulary } from "@/actions";
 import { Text } from "@/components/base";
+import { useCurrentUser } from "@/hooks";
 
 export default function QuizPage() {
   const { data: vocabulary } = useSWR("random-vocabulary", () =>
     findRandomVocabulary(),
   );
 
+  const { data: user, isLoading: isLoadingUser } = useCurrentUser();
+
   const router = useRouter();
 
   useEffect(() => {
-    if (vocabulary === null) {
+    if (!isLoadingUser && !user) {
+      toast.error("You have to login before quiz");
+      router.replace("/");
+    }
+    if (user && vocabulary === null) {
       router.replace("/vocabularies");
     }
-    if (vocabulary) {
+    if (user && vocabulary) {
       router.replace(`/quiz/${vocabulary.id}`);
     }
-  }, [vocabulary]);
+  }, [vocabulary, user, isLoadingUser, router]);
 
   return <Text>Loading</Text>;
 }
